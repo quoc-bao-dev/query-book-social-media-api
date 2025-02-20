95d885c693683182f05a52a6ad7967acefc430c9
Truong Quoc Bao (FPL HCM) <baotqps36791@fpt.edu.vn>
2025-02-20 15:58:00 +0700

    fix

--- qbook-social-media-node-js/src/router/question.routes.ts
+++ qbook-social-media-node-js/src/router/question.routes.ts
@@ -6,11 +6,7 @@ import { authMiddleware } from '../middlewares/authMiddleware';
 const questionRouter = Router();
 
 questionRouter.get('/', authMiddleware, wrapAsync(QuestionController.getAll));
-questionRouter.get(
-    '/:id',
-    authMiddleware,
-    wrapAsync(QuestionController.getByUserId)
-);
+questionRouter.get('/:id', wrapAsync(QuestionController.getByUserId));
 questionRouter.post('/', authMiddleware, wrapAsync(QuestionController.create));
 questionRouter.get(
     '/my-question',
